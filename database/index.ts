d30317a772e12c5c89fe2d1f79c46b67cfa8b55d
JEduardo <jesseard94@gmail.com>
2023-10-13 15:34:35 -0500

    Create REST, get summarized products and filters by gender.

--- database/index.ts
+++ database/index.ts
@@ -1,4 +1,7 @@
 export * as db from "./db";
 
 // Base de datos de prueba
-export * as seedDatabase from './products'
+export * as seedDatabase from "./products";
+
+// Constantes
+export * from "./constants";
