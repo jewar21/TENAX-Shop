3943aa92af1f1c5291fc3f1325bc8f30d49e49e4
JEduardo <jesseard94@gmail.com>
2023-10-13 15:02:14 -0500

    Resolved database connection problems and some problems with product models.

--- models/Product.ts
+++ models/Product.ts
@@ -3,31 +3,31 @@ import mongoose, { Schema, model, Model } from "mongoose";
 
 const productSchema = new Schema(
   {
-    description: { type: "string", required: true },
-    images: [{ type: "string" }],
+    description: { type: String, required: true },
+    images: [{ type: String }],
     inStock: { type: Number, required: true, default: 0 },
     price: { type: Number, required: true, default: 0 },
     sizes: [
       {
-        type: "string",
+        type: String,
         enum: {
           values: ["XS", "S", "M", "L", "XL", "XXL", "XXXL"],
           message: "{VALUE} no es un tamaño válido"
         }
       }
     ],
-    slug: { type: "string", required: true, unique: true },
-    tags: [{ type: "string" }],
-    title: { type: "string", required: true },
+    slug: { type: String, required: true, unique: true },
+    tags: [{ type: String }],
+    title: { type: String, required: true },
     type: {
-      type: "string",
+      type: String,
       enum: {
         values: ["shirts", "pants", "hoodies", "hats"],
         message: "{VALUE} no es un tipo válido"
       }
     },
     gender: {
-      type: "string",
+      type: String,
       enum: {
         values: ["men", "women", "kid", "unisex"],
         message: "{VALUE} no es un genero válido"
