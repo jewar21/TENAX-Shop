75dbb3cbca6f294c9074750786930e47313d4b49
JEduardo <jesseard94@gmail.com>
2023-10-13 18:49:37 -0500

    Created the context for the menu when it is closed or open

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import { CssBaseline, ThemeProvider } from '@mui/material'
 import { SWRConfig } from 'swr/_internal'
 import { lightTheme } from '../themes'
 import '@/styles/globals.css'
+import { UiProvider } from '@/context'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -11,10 +12,12 @@ export default function App({ Component, pageProps }: AppProps) {
         fetcher: (resource, init) => fetch(resource, init).then(res => res.json())
       }}>
 
-      <ThemeProvider theme={lightTheme}>
-        <CssBaseline />
-        <Component {...pageProps} />
-      </ThemeProvider>
+      <UiProvider>
+        <ThemeProvider theme={lightTheme}>
+          <CssBaseline />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </UiProvider>
 
     </SWRConfig>
 
